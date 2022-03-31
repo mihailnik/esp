175006dd421c17c61ab1431fe13f5047227b069a
mihailnik <mihailnikolayevich@gmail.com>
2022-03-31 21:29:09 +0300

    1

--- e32i2s_saps/src/main.cpp
+++ e32i2s_saps/src/main.cpp
@@ -1,11 +1,13 @@
 #include <Arduino.h>
 
+// i2s lib
 #include <SPIFFS.h>
 #include "WAVFileReader.h"
 #include "SinWaveGenerator.h"
 #include "I2SOutput.h"
+//end i2s lib
 
-// i2s pins
+// i2s lib
 i2s_pin_config_t i2sPins = {
     .bck_io_num = GPIO_NUM_26,
     .ws_io_num = GPIO_NUM_25,
@@ -14,11 +16,12 @@ i2s_pin_config_t i2sPins = {
 
 I2SOutput *output;
 SampleSource *sampleSource;
-
+//end i2s lib
 void setup()
 {
   Serial.begin(115200);
 
+// i2s lib
   Serial.println("Starting up");
 
   SPIFFS.begin();
@@ -32,6 +35,7 @@ void setup()
   Serial.println("Starting I2S Output");
   output = new I2SOutput();
   output->start(I2S_NUM_1, i2sPins, sampleSource);
+//end i2s lib
 }
 
 void loop()
