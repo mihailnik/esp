a530ebd7802564e28256e883d133ad2f0b0ca3c4
mihailnik <mihailnikolayevich@gmail.com>
2022-06-16 12:48:42 +0300

    1

--- 01_ex_generic/src/main.h
+++
@@ -1,8 +0,0 @@
-
-#include "globals.h"
-
-#define BAUD_RATE       115200  // serial debug port baud rate
-
-#ifndef LED_BUILTIN
-#define LED_BUILTIN     2 // please check PIN where LED attached
-#endif
